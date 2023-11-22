14f19555094ab965a2f5c8748432fc8cd9e3a584
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2023-11-22 11:45:16 +0500

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,13 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react';
-
-const CounterDisplay = ({ count }) => {
-  return <p className="text-4xl m-8">Count: {count}</p>;
-};
-
-const Counter = () => {
-  const [count, setCount] = useState(0);
-
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
-
-  const handleDecrement = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  };
-
-  const handleReset = () => {
-    setCount(0);
-  };
-
+import React from 'react'
+import { Router, Routes, Route } from 'react-router-dom';
+import Counter from "./components/state";
+const App = () => {
   return (
-    <> <div className='flex justify-center bg-gradient-to-r from-blue-500 to-transparent h-screen '>
-      <div className="text-center w-1/2 mt-52 h-56 rounded bg-pink-200">
-        <h1 className="text-3xl font-bold mb-4 mt-2">Counter Component</h1>
-        <CounterDisplay count={count} />
-        <button
-          onClick={handleIncrement}
-          className="bg-green-500  text-white px-8 py-2 rounded mr-2 mt-6"
-        >
-          Increment
-        </button>
-        <button
-          onClick={handleDecrement}
-          className="bg-red-500 text-white px-8 py-2 rounded mr-2"
-        >
-          Decrement
-        </button>
-        <button
-          onClick={handleReset}
-          className="bg-blue-500 text-white px-8 py-2 rounded"
-        >
-          Reset
-        </button>
-      </div>
-    </div>
+    <>
+      <Counter />
     </>
-  );
-};
+  )
+}
 
-export default Counter;
+export default App;
