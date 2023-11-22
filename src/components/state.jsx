cf68928648a8284581be14bef671f234ebe881e4
MudassarA10 <123820684+MudassarA10@users.noreply.github.com>
2023-11-22 18:46:32 +0500

    commmit

--- src/components/state.jsx
+++ src/components/state.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 
 const CounterDisplay = ({ count }) => {
-    return <p className="text-4xl m-8">Count: {count}</p>;
+    return <p className="text-2xl md:text-4xl mb-4 md:mb-8">Count: {count}</p>;
 };
 
 const Counter = () => {
@@ -24,31 +24,32 @@ const Counter = () => {
     };
 
     return (
-        <> <div className='flex justify-center bg-gradient-to-r from-blue-500 to-transparent h-screen '>
-            <div className="text-center w-1/2 mt-52 h-56 rounded bg-pink-200">
-                <h1 className="text-3xl font-bold mb-4 mt-2">Counter Component</h1>
+        <div className='flex justify-center items-center min-h-screen bg-gradient-to-r from-blue-500 to-transparent'>
+            <div className="flex-cols text-center w-full max-w-md md:max-w-lg p-4 md:p-8 bg-white rounded-lg shadow-md">
+                <h1 className="text-xl md:text-3xl font-bold mb-2 md:mb-4">Counter Component</h1>
                 <CounterDisplay count={count} />
-                <button
-                    onClick={handleIncrement}
-                    className="bg-green-500  text-white px-8 py-2 rounded mr-2 mt-6"
-                >
-                    Increment
-                </button>
-                <button
-                    onClick={handleDecrement}
-                    className="bg-red-500 text-white px-8 py-2 rounded mr-2"
-                >
-                    Decrement
-                </button>
-                <button
-                    onClick={handleReset}
-                    className="bg-blue-500 text-white px-8 py-2 rounded"
-                >
-                    Reset
-                </button>
+                <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-x-4 md:space-y-0">
+                    <button
+                        onClick={handleIncrement}
+                        className="bg-green-500 text-white px-4 md:px-6 py-2 rounded w-full md:w-36"
+                    >
+                        Increment
+                    </button>
+                    <button
+                        onClick={handleDecrement}
+                        className="bg-red-500 text-white px-4 md:px-6 py-2 rounded w-full md:w-36"
+                    >
+                        Decrement
+                    </button>
+                    <button
+                        onClick={handleReset}
+                        className="bg-blue-500 text-white px-4 md:px-6 py-2 rounded w-full md:w-36"
+                    >
+                        Reset
+                    </button>
+                </div>
             </div>
         </div>
-        </>
     );
 };
 
